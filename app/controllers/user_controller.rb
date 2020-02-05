ea7d1892cc264686e9398416db9982d494f0d343
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-05 15:08:32 +0000

    adding log in functionality

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -22,5 +22,23 @@ class UserController < ApplicationController
       redirect '/signup'
     end
   end
+  
+  get '/login' do
+    if logged_in?
+      redirect '/homepage'
+    else
+      erb :'user_controllers/login.html'
+    end
+  end
+  
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect '/tweets'
+      else 
+        redirect '/login'
+      end
+    end
 
 end
