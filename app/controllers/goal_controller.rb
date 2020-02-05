7b69ce87277d028d3e7ce24211b394b331f181bb
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-05 18:55:26 +0000

    creating route to make a new goal

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -1,13 +1,21 @@
 class GoalController < ApplicationController
   
   get '/goals' do
-        if logged_in?
-            @user = User.find_by_id(session[:user_id])
-            @goals = Goal.all
-            erb :'/goal_views/goals'
-        else
-            redirect '/login'
-        end
-    end 
+    if logged_in?
+      @user = User.find_by_id(session[:user_id])
+      @goals = Goal.all
+      erb :'/goal_views/goals'
+    else
+      redirect '/login'
+    end
+  end 
+  
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/goal_views/new'
+    else
+      redirect '/login'
+    end
+  end
     
 end
