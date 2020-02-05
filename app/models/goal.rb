54f1fa794a67022734f691991166ee0fe876e45c
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-05 17:57:47 +0000

    adding goals

---
+++ app/models/goal.rb
@@ -0,0 +1,4 @@
+class Goal < ActiveRecord::Base
+  belongs_to :user
+  validates_presence_of :goal_name, :description, :achieved?
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   has_secure_password
-  has_many :experiences
+  has_many :goals
   validates_presence_of :username, :email, :password
 end
