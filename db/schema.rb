9d861a5bbda6a9b233423c2e1872f94363f08530
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-05 18:02:51 +0000

    creating goals table in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200204191745) do
+ActiveRecord::Schema.define(version: 20200205180013) do
+
+  create_table "goals", force: :cascade do |t|
+    t.string "goal_name"
+    t.string "description"
+    t.string "achieved?"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
