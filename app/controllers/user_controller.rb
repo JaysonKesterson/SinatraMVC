30607775a989717e27f12ede9335261af33d1104
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-07 01:59:43 +0000

    associating goals with specific user

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -48,7 +48,7 @@ class UserController < ApplicationController
   
   get '/users/:id' do
     if logged_in?
-      @user = User.find_by_id(session[:user_id])
+      @user = current_user
       @user_goals = Goal.all.select{|goal| goal.user_id == current_user.id}   
       erb :'/user_views/show'
     else
