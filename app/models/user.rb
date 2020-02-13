2828a1eab1be87cf78d6ac4bf0506b961a42b1af
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-13 18:22:12 +0000

    changing validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :goals
-  validates :username, presence: true, uniqueness: true, length: { minimum: 2 }
-  validates :email, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :username, presence: true, uniqueness: true, length: { in: 2..20}
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { in: 6..15 }
 end
