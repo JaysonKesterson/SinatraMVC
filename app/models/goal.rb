a341791c2ab5b207d05a02de29f2d47bb4721de0
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-13 00:33:29 +0000

    adding maximum character limits to limit bad data input

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,6 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
-  validates :goal_name, presence: true, length: { minimum: 3 }
-  validates :description, presence: true, length: { minimum: 3 }
+  validates :goal_name, presence: true, length: { in: 6..50 }
+  validates :description, presence: true, length: { in: 6..200 }
   validates :achieved, presence: true
 end
