392eb9e424f0b782dd3ad48926ac038dd18d9a4d
Jayson Kesterson <jayson.kesterson24@gmail.com>
2020-02-15 20:21:10 +0000

    changing requirements on goal validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,6 @@
 class Goal < ActiveRecord::Base
   belongs_to :user
-  validates :goal_name, presence: true, length: { in: 6..50 }
-  validates :description, presence: true, length: { in: 6..200 }
+  validates :goal_name, presence: true, length: { in: 1..50 }
+  validates :description, presence: true, length: { in: 1..200 }
   validates :achieved, presence: true
 end
